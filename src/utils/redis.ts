8300155899a4c2271675b2bf183d6cf0695bd8b7
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-28 18:03:14 +0700

    Update redis.ts

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,5 +1,10 @@
 import { createClient } from "redis";
-export const redis = createClient();
+export const redis = createClient({
+  socket: {
+    host: process.env.REDIS_URL,
+    port: 6379
+  }
+});
 
 export const redixConnect = async () => {
   await redis.connect();
